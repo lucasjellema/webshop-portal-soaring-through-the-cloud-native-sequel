8094e618668b4e64018d2f8bbe594562de1e22bb
Lucas Jellema <lucasjellema@gmail.com>
2018-02-15 12:51:02 +0100

    Wait between command, first pull from GitHub

--- build-app.sh
+++ build-app.sh
@@ -1,13 +1,19 @@
- git clone https://github.com/lucasjellema/webshop-portal-soaring-through-the-cloud-native-sequel
+#git clone https://github.com/lucasjellema/webshop-portal-soaring-through-the-cloud-native-sequel
+# cd webshop-portal-soaring-through-the-cloud-native-sequel
 
- cd webshop-portal-soaring-through-the-cloud-native-sequel
- npm install
- ojet build --release
-
-cp -a ./web/. ./jet-on-node/public
-
-cd jet-on-node
+git pull
+wait
 
 npm install
-
+wait
+ojet build --release
+wait
+cp -a ./web/. ./jet-on-node/public
+wait
+cd jet-on-node
+wait
+npm install
+wait
 zip -r webshop-portal-soaring-through-the-cloud-native-sequel.zip .
+
+
